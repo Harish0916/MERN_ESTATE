81b989b7daf739bbb71f54c42321b0a139a41d1c
Harish Ojha <harish0916ojhacs@gmail.com>
2024-06-14 02:03:19 +0530

    Created Api for SignUp btn

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 const app = express();
 const PORT = 3000;
@@ -10,8 +11,10 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_DB)
     .then(()=>console.log("Connected to MongoDB"))
     .catch((err)=>console.log(err))
-    
+
+app.use(express.json());
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`)
