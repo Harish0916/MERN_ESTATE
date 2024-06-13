f1179ede31b3eb6cdfcc87e70bf3cac585922c30
Harish Ojha <harish0916ojhacs@gmail.com>
2024-06-14 00:50:15 +0530

    Complete Database Setup

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 const app = express();
 const PORT = 3000;
+dotenv.config();
 
+mongoose.connect(process.env.MONGO_DB)
+    .then(()=>console.log("Connected to MongoDB"))
+    .catch((err)=>console.log(err))
+    
 app.get("/", (req, res)=>{
     res.status(200).send("Hello")
 })
